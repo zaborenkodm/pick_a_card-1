f9905109d008c890d684bde309dca3e5a6f4cb95
aristofun <aristofun@yandex.ru>
2015-07-15 17:47:46 +0400

    initial commit

---
+++ pick_a_card.rb
@@ -0,0 +1,5 @@
+
+values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+
+puts "#{values.sample} of #{suits.sample}"
